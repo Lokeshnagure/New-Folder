4c305315a7984d405e214a2bd8c1fa54b2797612
LOKinaGuRE <94413037+LOKinaGuRE@users.noreply.github.com>
2022-09-14 16:52:47 +0530

    initial commit

---
+++ SumProgram.java
@@ -0,0 +1,11 @@
+public class SumProgram {
+    public static void main(String ar[]){
+        
+        int a = 1;
+        int b = 3;
+
+        int sum = a + b ;
+
+        System.out.println("SUM : " + sum);
+    }
+}
