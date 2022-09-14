7a440d1a35ebae2f7dfe3ac3be4ec83eaa52c53b
LOKinaGuRE <94413037+LOKinaGuRE@users.noreply.github.com>
2022-09-14 17:35:33 +0530

    LeapYear code sucessfully ran and Staged LeapYear

---
+++ LeapYear.java
@@ -0,0 +1,24 @@
+
+import java.lang.*;
+import java.util.*;
+
+public class LeapYear {
+    public static void main(String arg[]){
+
+        int year ;
+        Scanner sc = new Scanner (System.in);
+
+        System.out.println("Enter the Year : ");
+        year = sc.nextInt();
+
+      if((year % 4==0) || ((year %400 == 0)&& (year %100 !=0)) ) {
+        System.out.println("Leap Year : " + year);
+
+      }
+      else System.out.println("Not Leap Year : " + year);
+
+
+
+    }
+
+}
