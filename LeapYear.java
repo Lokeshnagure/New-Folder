2d9b345ff68213a35b321ee82ad88dbd14a3f4ca
LOKinaGuRE <94413037+LOKinaGuRE@users.noreply.github.com>
2022-09-14 17:46:27 +0530

    Added s.close(); in LeapYear

--- LeapYear.java
+++ LeapYear.java
@@ -10,6 +10,7 @@ public class LeapYear {
 
         System.out.println("Enter the Year : ");
         year = sc.nextInt();
+        sc.close();
 
       if((year % 4==0) || ((year %400 == 0)&& (year %100 !=0)) ) {
         System.out.println("Leap Year : " + year);
