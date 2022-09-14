c212960c96cd72f9af6beaacc62106475edc3659
LOKinaGuRE <94413037+LOKinaGuRE@users.noreply.github.com>
2022-09-14 17:37:29 +0530

    In Sum code, scanner constructer closeed

--- SumProgram.java
+++ SumProgram.java
@@ -1,9 +1,15 @@
+import java.util.*;
+
 public class SumProgram {
     public static void main(String ar[]){
         
-        int a = 1;
-        int b = 3;
-
+        int a ;
+        int b ;
+        Scanner sc = new Scanner(System.in);
+        a=sc.nextInt();
+        b =sc.nextInt();
+        sc.close();
+        System.out.println("Please Enter Number To Do SUM :");
         int sum = a + b ;
 
         System.out.println("SUM : " + sum);
